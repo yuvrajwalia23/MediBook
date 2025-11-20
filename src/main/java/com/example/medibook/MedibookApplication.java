4f36adef7aba2e61c84634a0450f0b9cbe68dbdf
yuvrajwalia23 <yuvrajwalia2023@gmail.com>
2025-11-20 21:27:12 +0530

    first commit -- project pushed to github

---
+++ src/main/java/com/example/medibook/MedibookApplication.java
@@ -0,0 +1,13 @@
+package com.example.medibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedibookApplication.class, args);
+	}
+
+}
